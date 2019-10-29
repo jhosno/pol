3ec3d7cb261dfa2ef5c25ff44f1d7c5687fa09c0
Jhosno <jhosnoirlit@gmail.com>
2019-10-28 23:05:40 -0430

    write on json file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,46 @@
-const { Router } = require('express');
- 
+const {
+  Router
+} = require('express');
+
 const router = Router();
 
-const records = [];
+const uuid = require('uuid/v4');
+
+const fs = require('fs');
+
+const jsonRecords = fs.readFileSync('src/records.json','utf-8')
+let records = JSON.parse(jsonRecords);
+let today = new Date();
+
 
 //con el router crear un el metodo get una ruta inicial 
 router.get('/', (req, res) => {
-    res.render('index.ejs');
+  res.render('index.ejs', {
+    records
   });
- 
+});
+
 router.get('/new-record', (req, res) => {
   res.render('new-record.ejs')
 })
-router.post('/new-record', (req, res) => {
-  console.log(req.body);
-  const message = 'Your record is saved! :)';
-  res.render('index.ejs').send(message);
-})
 
+//min 40 para las validaciones 
+router.post('/new-record', (req, res) => {
+  const {
+    monotony,
+    quote
+  } = req.body;
+  let newRecord = {
+    id: uuid(),
+    date: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(),
+    time: today.getHours(),
+    monotony,
+    quote
+  };
+  records.push(newRecord);
+  const jsonRecords = JSON.stringify(records);
+  fs.writeFileSync('src/records.json', jsonRecords, 'utf-8');
+  res.redirect('/');
+})
+//1'00"45s
 module.exports = router;
